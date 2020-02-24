564e0b205e345c3f65b8172d655b88440343d5d3
ashutosh321607 <ashutoshg@iitbhilai.ac.in>
2020-02-24 13:56:47 +0530

    sign up page added still working on responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { FindworkspaceComponent } from './findworkspace/findworkspace.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: 'signin', component: SigninComponent },
+	{path: 'find', component: FindworkspaceComponent },
+	{path: 'signup', component: SignupComponent },
+
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
