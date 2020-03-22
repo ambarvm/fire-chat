ee7145339c2ca3d0f1fce803bba5df209ec50046
tapeshmathur <60978989+tapeshmathur@users.noreply.github.com>
2020-03-22 21:34:38 +0530

    Add side-nav and profile component (#2)
    * second2
    
    * changes
    
    * 🐛Bug fix and 🧹code cleaning
    - refactor code so that both drawer and profile work
    - remove unused typescript and css
    - reformat existing code
    
    * update profile component
    - use imageURL as input
    - remove repetetive info
    - remove unused image
    
    * arrange imports in app module
    
    * update fomatting
    
    Co-authored-by: Ambar Mutha <ambar.ytl@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	declarations: [AppComponent, NavBarComponent, ProfileComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+		FormsModule,
+		MatCardModule,
+		MatButtonModule,
+		MatDividerModule,
+		MatListModule,
+		MatExpansionModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
