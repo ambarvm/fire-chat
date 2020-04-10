3732d0450b70d76aba1187cade5b0a49ea76dcf8
himanimadaan@github.com <himanimadaan2025@gmail.com>
2020-04-10 11:46:44 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+
+import { ChatService } from './services/chat.service';
+import { AuthService } from './services/auth.service';
+
+import { appRoutes } from '../routes';
+import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { PickerModule } from '@ctrl/ngx-emoji-mart';
+//import  {  NgxEmojiPickerModule  }  from  'ngx-emoji-picker';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    ChatFormComponent,
+    ChatroomComponent,
+    FeedComponent,
+    MessageComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserItemComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    //PickerModule
+    //NgxEmojiPickerModule.forRoot(),
+  ],
+  providers: [AuthService, ChatService],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
